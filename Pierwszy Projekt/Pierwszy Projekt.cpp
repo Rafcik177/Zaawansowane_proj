5447361cb1d8b342068e845ffbf186e389a8340e
Rafcik177 <92384978+Rafcik177@users.noreply.github.com>
2021-11-08 20:07:32 +0100

    ver. 2.0 Obliczanie funkcji kwadratowej w ax^2+bx+c a=/=0

--- Pierwszy Projekt/Pierwszy Projekt.cpp
+++ Pierwszy Projekt/Pierwszy Projekt.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int main()
     cout << "Wybierz odpowiednia opcje:\n";
     cout << "1. Obliczanie f. liniowej w postaci y=ax+b"<<endl;
     cout << "2. Obliczanie f. liniowej w postaci Ax+By+c=0"<<endl;
+    cout << "3. Obliczanie f. kwadratowej w postaci ax^2+bx+c a=/=0"<<endl;
     cin >> wybor;
     if (wybor == 1)
     {
@@ -48,6 +50,33 @@ int main()
                 cout << "Funkcja nie ma miejsc zerowych" << endl;
         }
     }
-    
+    else if (wybor == 3)
+    {
+        float a,b,c,x1,x2,delta;
+        cout << "Obliczanie f. kwadratowej w postaci ax^2+bx+c a=/=0!" << endl;
+        cout << "Podaj A: "; cin >> a;
+        cout << "Podaj B: "; cin >> b;
+        cout << "Podaj C: "; cin >> c;
+        if (a != 0)
+        {
+            delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                x1 = (-b - sqrt(delta)) / (2 * a);
+                x2 = (-b + sqrt(delta)) / (2 * a);
+                cout << "Funkcja ma dwa miejsca zerowe:\n";
+                cout << "x1: " << x1 << endl;
+                cout << "x2: " << x2 << endl;
+            }
+            else if (delta == 0)
+            {
+                x1 = -b/ (2 * a);
+                cout << "Funkcja ma tylko jedno miejsce zerowe:\n";
+                cout << "x0: " << x1 << endl;
+            }
+            else cout << "Funkcja nie ma miejsc zerowych.\n";
+        }
+
+    }
 
 }
