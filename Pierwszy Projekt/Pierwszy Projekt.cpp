8568356dcd84f415297ba4801740e8dca79c5cc0
Rafcik177 <92384978+Rafcik177@users.noreply.github.com>
2021-11-08 19:07:43 +0100

    Wczytanie plików na gita

---
+++ Pierwszy Projekt/Pierwszy Projekt.cpp
@@ -0,0 +1,9 @@
+﻿// Pierwszy Projekt.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
