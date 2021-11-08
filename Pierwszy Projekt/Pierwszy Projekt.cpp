53aab0ec232c2b2f1c9b3ae954437a02db36ded9
Rafcik177 <92384978+Rafcik177@users.noreply.github.com>
2021-11-08 19:52:18 +0100

    ver. 1.4 Obliczanie miejsc zerowych postaci Ax+By+c=0

--- Pierwszy Projekt/Pierwszy Projekt.cpp
+++ Pierwszy Projekt/Pierwszy Projekt.cpp
@@ -4,20 +4,50 @@ using namespace std;
 
 int main()
 {
-    float a, b, x0;
-    cout << "Obliczanie miejsca zerowego f. liniowej! (a =/= 0)" << endl;
-    cout << "Podaj a: "; cin >> a;
-    cout << "Podaj b: "; cin >> b;
-    if (a != 0)
+    int wybor;
+    cout << "Wybierz odpowiednia opcje:\n";
+    cout << "1. Obliczanie f. liniowej w postaci y=ax+b"<<endl;
+    cout << "2. Obliczanie f. liniowej w postaci Ax+By+c=0"<<endl;
+    cin >> wybor;
+    if (wybor == 1)
     {
-        x0 = -b / a;
-        cout << "Miejsce zerowe: " << x0;
+        float a, b, x0;
+        cout << "Obliczanie miejsca zerowego f. liniowej y=ax+b! (a =/= 0)" << endl;
+        cout << "Podaj a: "; cin >> a;
+        cout << "Podaj b: "; cin >> b;
+        if (a != 0)
+        {
+            x0 = -b / a;
+            cout << "Miejsce zerowe: " << x0;
+        }
+        else {
+            if (b == 0)
+                cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
+            else
+                cout << "Funkcja nie ma miejsc zerowych" << endl;
+        }
     }
-    else {
-        if (b == 0)
-            cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
-        else
-            cout << "Funkcja nie ma miejsc zerowych" << endl;
+    else if (wybor == 2)
+    {
+        float a, b, c, A, B, C, x0;
+        cout << "Obliczanie miejsca zerowego f. liniowej Ax+By+c=0!" << endl;
+        cout << "Podaj A: "; cin >> A;
+        cout << "Podaj B: "; cin >> B;
+        cout << "Podaj C: "; cin >> C;
+        a = -A / B;
+        c = -C / B;
+        if (a != 0)
+        {
+            x0 = -c / a;
+            cout << "Miejsce zerowe: " << x0;
+        }
+        else {
+            if (c == 0)
+                cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
+            else
+                cout << "Funkcja nie ma miejsc zerowych" << endl;
+        }
     }
+    
 
 }
