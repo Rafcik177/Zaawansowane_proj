c02e1b49188be47deb068ec86c482a6f6577adcb
Rafcik177 <92384978+Rafcik177@users.noreply.github.com>
2021-11-09 12:05:24 +0100

    ver 2.6 Obliczanie miejsc zerowych postaci kanonicznej"

--- Pierwszy Projekt/Pierwszy Projekt.cpp
+++ Pierwszy Projekt/Pierwszy Projekt.cpp
@@ -8,10 +8,9 @@ int main()
     int wybor;
     cout << "=======================================================" << endl;
     cout << "Wybierz odpowiednia opcje:"<<endl;
-    cout << "1. Obliczanie x0 f. liniowej w postaci y=ax+b"<<endl;
-    cout << "2. Obliczanie x0 f. liniowej w postaci Ax+By+c=0"<<endl;
-    cout << "3. Obliczanie x0 f. kwadratowej w postaci ax^2+bx+c a=/=0"<<endl;
-    cout << "4. Obliczanie x0 f. kwadratowej w postaci y=a(x-p)^2+q"<<endl;
+    cout << "1. Obliczanie f. liniowej w postaci y=ax+b"<<endl;
+    cout << "2. Obliczanie f. liniowej w postaci Ax+By+c=0"<<endl;
+    cout << "3. Obliczanie f. kwadratowej w postaci ax^2+bx+c a=/=0"<<endl;
     cout << "======================================================="<<endl;
     cin >> wybor;
     if (wybor == 1)
@@ -95,50 +94,5 @@ int main()
             }
         }
 
-    }
-    else if (wybor == 4)
-    {
-        float a, p, q,b,c, x1, x2;
-        cout << "Obliczanie f. kwadratowej w postaci y=a(x-p)^2+q!" << endl;
-        cout << "Podaj a: "; cin >> a;
-        cout << "Podaj p: "; cin >> p;
-        cout << "Podaj q: "; cin >> q;
-        b = -2 * a * p;
-        c = a*p*p+q;
-        if (a != 0)
-        {
-            delta = b * b - 4 * a * c;
-            if (delta > 0)
-            {
-                x1 = (-b - sqrt(delta)) / (2 * a);
-                x2 = (-b + sqrt(delta)) / (2 * a);
-                cout << "Funkcja ma dwa miejsca zerowe:\n";
-                cout << "x1: " << x1 << endl;
-                cout << "x2: " << x2 << endl;
-            }
-            else if (delta == 0)
-            {
-                x1 = -b / (2 * a);
-                cout << "Funkcja ma tylko jedno miejsce zerowe:\n";
-                cout << "x0: " << x1 << endl;
-            }
-            else cout << "Funkcja nie ma miejsc zerowych.\n";
-        }
-        else
-        {
-            cout << "To jest funkcja liniowa" << endl;
-            if (b != 0)
-            {
-                x1 = -c / b;
-                cout << "Miejsce zerowe: " << x1;
-            }
-            else {
-                if (c == 0)
-                    cout << "Funkcja ma nieskonczenie wiele miejsc zerowych" << endl;
-                else
-                    cout << "Funkcja nie ma miejsc zerowych" << endl;
-            }
-        }
-       
     }
 }
